6049754353bcef67cfd0498693368ed0f8eb4061
pragmaticAweds <jimohafeez738@gmail.com>
2024-04-16 11:57:06 +0100

    chore: added deployment to github action workflow

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get("/version", (req, res) => {
   res.send("1"); // change this string to ensure a new version deployed
 });
 
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+
+  res.send("ok");
+});
+
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
