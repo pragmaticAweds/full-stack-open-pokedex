895eae563bfcc88dc3b8010668a9e824a1070fc2
pragmaticAweds <jimohafeez738@gmail.com>
2024-04-16 12:07:37 +0100

    chore: added deployment to github action workflow

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
 });
