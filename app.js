cd36887859f344118391c313b6e931cba3701a37
pragmaticAweds <jimohafeez738@gmail.com>
2024-05-14 18:26:24 +0100

    chore: health check implementation

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get("/version", (req, res) => {
 
 app.get("/health", (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  // if (true) throw "error...  ";
-  res.send("ping");
+  if (true) throw "error...  ";
+  // res.send("ping");
 });
 
 // eslint-disable-next-line no-undef
